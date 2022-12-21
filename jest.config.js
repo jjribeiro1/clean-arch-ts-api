4a61e62966ca82c171cbfebb08c48de77c2694dd
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-21 19:27:58 -0300

    chore: add jest --watch e config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  collectCoverage: true,
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
 };
-
