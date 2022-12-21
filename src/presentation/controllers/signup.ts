bfbd809b490f3121e6855d12db67f3563d45ecce
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-21 19:31:54 -0300

    feat: signupController

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,15 @@
+import { IHttpRequest, IHttpResponse } from '../protocols/http';
+import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helper/http-helper';
+
+export class SignUpController {
+  handle(httpRequest: IHttpRequest): IHttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+  }
+}
