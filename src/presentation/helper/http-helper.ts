69d2ca822610947dbdc5f14332c8281bc70ef082
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-22 00:31:54 -0300

    feat: add helper ok for success requests

--- src/presentation/helper/http-helper.ts
+++ src/presentation/helper/http-helper.ts
@@ -8,5 +8,10 @@ export const badRequest = (error: Error): IHttpResponse => ({
 
 export const serverError = (): IHttpResponse => ({
   statusCode: 500,
-  body: new ServerError()
+  body: new ServerError(),
+});
+
+export const ok = (data: any): IHttpResponse => ({
+  statusCode: 200,
+  body: data,
 });
