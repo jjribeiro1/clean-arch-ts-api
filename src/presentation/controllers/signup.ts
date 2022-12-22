b9b45c92ae4427d848a60ec84de980a7f9cd7561
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-21 23:26:49 -0300

    feat: validating passwordConfirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -19,7 +19,13 @@ export class SignUpController implements IController {
         }
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      const { email, password, passwordConfirmation } = httpRequest.body;
+
+      if (passwordConfirmation !== password) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+
+      const isValid = this.emailValidator.isValid(email);
 
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
