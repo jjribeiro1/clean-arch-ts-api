4184c92a1f65abe5a39d5c38166c96bee753b357
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-21 21:40:35 -0300

    refactor: jest spyon EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,20 +14,10 @@ const makeEmailValidator = (): IEmailValidator => {
     }
   }
   return new EmailValidatorStub();
-}
-
-const makeEmailValidatorWithError = (): IEmailValidator => {
-  class EmailValidatorStub implements IEmailValidator {
-    isValid(email: string): boolean {
-      throw new Error();
-    }
-  }
-
-  return new EmailValidatorStub();
 };
 
 const makeSut = (): ISutTypes => {
-  const emailValidatorStub = makeEmailValidator()
+  const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
   return {
     sut,
@@ -125,8 +115,10 @@ describe('Signup Controller', () => {
   });
 
   test('Should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError();
-    const sut = new SignUpController(emailValidatorStub);
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error();
+    });
     const httpRequest = {
       body: {
         name: 'name',
