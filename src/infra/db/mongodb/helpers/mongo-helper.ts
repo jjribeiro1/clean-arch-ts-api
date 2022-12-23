f904eff3aa4cba94c848cf88ab6a4d8309fe4515
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-23 00:54:24 -0300

    refactor: mapping mongodb data in mongohelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,26 @@
-import { MongoClient } from 'mongodb';
-import { disconnect } from 'process';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
-  client: null as MongoClient,
+  client: null as unknown as MongoClient,
+  uri: null as unknown as string,
 
-  async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL);
+  async connect(uri: string) {
+    this.client = await MongoClient.connect(uri);
   },
 
-  async disconnect(): Promise<void> {
+  async disconnect() {
     await this.client.close();
+  },
+
+  getCollection(collectionName: string): Collection {
+    return this.client.db().collection(collectionName);
+  },
+  map: (data: any) => {
+    const { _id, ...rest } = data;
+    return { ...rest, id: _id.toHexString() };
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map((c) => MongoHelper.map(c));
   },
 };
