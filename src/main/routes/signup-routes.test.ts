30355bdae4f3e48c5870eb2fce88de7cd969f577
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-23 17:57:50 -0300

    test: should return an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app).post('/api/signup')
+    .send({
+        name: 'Joao',
+        email: 'joao@mail.com',
+        password: '123',
+        passwordConfirmation: '123'
+    })
+    .expect(200);
+  });
+});
