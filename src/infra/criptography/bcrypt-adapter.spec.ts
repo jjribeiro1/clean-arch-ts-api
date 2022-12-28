5db21a4b0c454277bf9f61a7c6ca6a2096dcaaf5
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-27 22:53:35 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -3,11 +3,11 @@ import { BcryptAdapter } from './bcrypt-adapter';
 
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
-    return Promise.resolve('hash')
+    return Promise.resolve('hash');
   },
 
   async compare(): Promise<boolean> {
-    return Promise.resolve(true)
+    return Promise.resolve(true);
   },
 }));
 
@@ -39,14 +39,23 @@ describe('Bcrypt Adapter', () => {
 
   test('Should call compare with correct values', async () => {
     const sut = makeSut();
-    const compareSpy = jest.spyOn(bcrypt, 'compare');
+    const isValidSpy = jest.spyOn(bcrypt, 'compare');
     await sut.compare('any_value', 'any_hash');
-    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
+    expect(isValidSpy).toHaveBeenCalledWith('any_value', 'any_hash');
   });
 
   test('Should return true when compare succeeds', async () => {
     const sut = makeSut();
-    const compare = await sut.compare('any_value', 'any_hash');
-    expect(compare).toBe(true);
+    const isValid = await sut.compare('any_value', 'any_hash');
+    expect(isValid).toBe(true);
+  });
+
+  test('Should return false when compare fails', async () => {
+    const sut = makeSut();
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+      return Promise.resolve(false)
+    })
+    const isValid = await sut.compare('any_value', 'any_password');
+    expect(isValid).toBe(false);
   });
 });
